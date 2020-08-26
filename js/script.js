dafc82a4a57975fc8b61f14f8987877173a3f5a4
Andrea Contestabile <andreacontest@gmail.com>
2020-08-26 15:37:18 +0200

    Test FizzBuzz JS (while)

--- js/script.js
+++ js/script.js
@@ -24,3 +24,21 @@ for (var i = 1; i <= 100; i++) {
   }
 
 }
+
+
+// var i = 1;
+// while (i < 101) {
+//
+//   if ((i % 3 == 0) && (i % 5 == 0)) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 == 0) {
+//     console.log("Fizz")
+//   } else if (i % 5 == 0) {
+//     console.log("Buzz")
+//   } else {
+//     console.log(i);
+//   }
+//
+//   i++;
+//
+// }
